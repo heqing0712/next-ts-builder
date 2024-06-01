9e66676dfc1f87564216fc35e5c8fba428fe8180
kenny.li <kenny.li@aibit-tech.com>
2024-06-02 03:54:36 +0800

    fix:下拉组件安装

--- src/components/home/builder-tools.tsx
+++ src/components/home/builder-tools.tsx
@@ -1,9 +1,9 @@
-import Arrow from '@/assets/icons/arrow.svg'
-import Hand from '@/assets/icons/hand.svg'
-import Message from '@/assets/icons/message.svg'
-import ThemeLight from '@/assets/icons/themeLight.svg'
-import ThemeDark from '@/assets/icons/themeLight.svg'
-import { BuilderToolsEnum } from '@/configs/enums/BuilderToolsEnum'
+import Arrow from "@/assets/icons/arrow.svg";
+import Hand from "@/assets/icons/hand.svg";
+import Message from "@/assets/icons/message.svg";
+import ThemeLight from "@/assets/icons/themeLight.svg";
+import ThemeDark from "@/assets/icons/themeLight.svg";
+import { BuilderToolsEnum } from "@/configs/enums/BuilderToolsEnum";
 import {
   Select,
   SelectContent,
@@ -12,76 +12,103 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { useTheme } from "next-themes"
-import { useState } from 'react'
+} from "@/components/ui/select";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import React from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
 export function BuilderTools() {
-  const [activeTool,setActiveTool] = useState(BuilderToolsEnum.ARROW)
- 
-  const { theme, setTheme } = useTheme()
- 
-  
+  const [activeTool, setActiveTool] = useState(BuilderToolsEnum.ARROW);
+
+  const { theme, setTheme } = useTheme();
+
   function getThemeDom() {
-    if( theme === 'dark'){
-      return <ThemeLight onClick={() => setTheme("light")}/>
-      }else{
-       return  <ThemeDark onClick={() => setTheme("dark")} />
-      }
+    if (theme === "dark") {
+      return <ThemeLight onClick={() => setTheme("light")} />;
+    } else {
+      return <ThemeDark onClick={() => setTheme("dark")} />;
+    }
   }
-    return <>
-    <div className="builder-tools">
+
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false);
+  const [showPanel, setShowPanel] = React.useState<Checked>(false);
+
+  return (
+    <>
+      <div className="builder-tools">
         <div className="tools-item">
-          <div  className={['tools-icon', activeTool === BuilderToolsEnum.ARROW ? 'is-active' : ''].join(' ')} >
-          <Arrow /> 
+          <div
+            className={[
+              "tools-icon",
+              activeTool === BuilderToolsEnum.ARROW ? "is-active" : "",
+            ].join(" ")}
+          >
+            <Arrow />
           </div>
-       
-            </div>
+        </div>
         <div className="tools-item">
-   
-          <div  className={['tools-icon', activeTool === BuilderToolsEnum.HAND ? 'is-active' : ''].join(' ')} >
-          <Hand />
+          <div
+            className={[
+              "tools-icon",
+              activeTool === BuilderToolsEnum.HAND ? "is-active" : "",
+            ].join(" ")}
+          >
+            <Hand />
           </div>
-            </div>
+        </div>
         <div className="tools-item">
-   
-          <div  className={['tools-icon', activeTool === BuilderToolsEnum.MESSAGE ? 'is-active' : ''].join(' ')} >
-          <Message />
+          <div
+            className={[
+              "tools-icon",
+              activeTool === BuilderToolsEnum.MESSAGE ? "is-active" : "",
+            ].join(" ")}
+          >
+            <Message />
           </div>
-            </div>
+        </div>
         <div className="tools-item">
-          <div className={['tools-icon', activeTool === BuilderToolsEnum.THEME ? 'is-active' : ''].join(' ')} >
-             
-            {
-              theme === 'dark' ?
-              <ThemeLight onClick={() => setTheme("light")} /> :
+          <div
+            className={[
+              "tools-icon",
+              activeTool === BuilderToolsEnum.THEME ? "is-active" : "",
+            ].join(" ")}
+          >
+            {theme === "dark" ? (
+              <ThemeLight onClick={() => setTheme("light")} />
+            ) : (
               <ThemeDark onClick={() => setTheme("dark")} />
-            }
-        
- 
+            )}
           </div>
         </div>
 
-        <div className="tools-item">
-        <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+        <div className="tools-item selectZoom">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Open</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="start">
+              <DropdownMenuCheckboxItem
+                checked={showPanel}
+                onCheckedChange={setShowPanel}
+              >
+                Panel
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-        
-
-
-
       </div>
     </>
+  );
 }
